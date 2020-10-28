b4ad8f6c972eeff5ccc6e0f77c61fc36890bef98
Guille Cosano <i62cogag@uco.es>
2020-10-28 14:23:06 +0100

    Created class BearComponent

---
+++ src/components/BearComponent.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+
+type Props = {};
+
+type State = {
+    bears: number;
+};
+
+export class BearComponent extends React.Component<Props, State>{
+    constructor(props: {}){
+        super(props);
+        this.state = { bears: 0 };
+    }
+
+    render(){
+        return(
+            <>
+                <span> {`${this.state.bears} Bears`} </span>
+
+                <button
+                data-testid="increase-button"
+                onClick={() => {
+                    this.setState({bears: this.state.bears + 1});
+                }}
+                >
+                Addopt a new bear!
+                </button>
+
+                <button
+                data-testid="decrease-button"
+                onClick={() => {
+                    this.setState({bears: this.state.bears - 1});
+                }}
+                >
+                Addopt a new bear!
+                </button>
+            </>
+        );
+    }
+
+}
